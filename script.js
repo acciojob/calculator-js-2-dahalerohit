a54e426f08c56243ae48b1174939fd9b3b1eae17
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-12-27 01:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const display = document.querySelector("#display");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const id = btn.id;
+    const value = btn.innerText;
+
+    if (id === "c") {
+      display.innerText = "";
+    }
+    else if (id === "back") {
+      display.innerText = display.innerText.slice(0, -1);
+    }
+    else if (id === "equal") {
+      try {
+        display.innerText = eval(display.innerText);
+      } catch {
+        display.innerText = "Error";
+      }
+    }
+    else {
+      display.innerText += value;
+    }
+  });
+});
